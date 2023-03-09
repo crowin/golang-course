b0fdf7a63ef069e0b7654f7133877c4ddd38203e
Alexander Stolnikov <crowin@users.noreply.github.com>
2023-03-09 14:51:52 +0100

    moved to none blocked execution

--- cache.go
+++ cache.go
@@ -1,45 +1,85 @@
 package cache
 
 import (
+	"fmt"
+	"sync"
 	"time"
 )
 
 type Cache struct {
-	data map[string]Value
+	data  map[string]Value
+	mutex sync.RWMutex
+	quit  chan struct{}
 }
 
 type Value struct {
 	value      any
-	expiration time.Time
+	expiration int64
 }
 
 func (c *Cache) Set(key string, value any) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	c.data[key] = Value{
 		value:      value,
-		expiration: time.Now().Add(time.Hour * time.Duration(24)),
+		expiration: time.Now().Add(time.Second * time.Duration(15)).UnixMilli(),
 	}
 }
 
 func (c *Cache) Get(key string) (value any, found bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	result, exist := c.data[key]
 	if !exist {
-		return value, false
-	} else if result.expiration.Before(time.Now()) {
-		delete(c.data, key)
-		return value, false
-	} else {
-		return result.value, true
+		return nil, false
+	} else if time.Now().UnixMilli() > result.expiration {
+		c.remove(key)
+		return nil, false
+	}
+
+	return result.value, true
+}
+
+func (c *Cache) remove(key string) {
+	fmt.Printf("Value with key %s will be deleted\n", key)
+	delete(c.data, key)
+}
+
+func (c *Cache) cleanUp() {
+	ticker := time.NewTicker(time.Second * 5)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			c.mutex.Lock()
+			fmt.Printf("Cleaning is starting\n")
+
+			for k, v := range c.data {
+				if time.Now().UnixMilli() > v.expiration {
+					c.remove(k)
+				}
+			}
+
+			c.mutex.Unlock()
+		case <-c.quit:
+			return
+		}
 	}
 }
 
-func (c *Cache) Remove(key string) {
-	delete(c.data, key)
+func (c *Cache) StopCleanUp() {
+	close(c.quit)
+	fmt.Println("Stop cleaning")
 }
 
 func NewCache() *Cache {
 	c := &Cache{
 		data: make(map[string]Value),
+		quit: make(chan struct{}),
 	}
+
+	go c.cleanUp()
 
 	return c
 }
